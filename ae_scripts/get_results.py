32682ce8531e07a468dd420388cc6f3ec9341624
Meng <mengwang@ece.ubc.ca>
2025-03-28 13:48:40 -0700

    custom testing added

--- ae_scripts/get_results.py
+++ ae_scripts/get_results.py
@@ -2,32 +2,46 @@ from tqsim import get_command
 import subprocess
 from tqdm import tqdm
 import json
+import sys
 
 def main():
-    classes = ['adder', 'bv', 'mul', 'qaoa', 'qft', 'qpe', 'qsc', 'qv']
-    max_qubits = 13
+    
+    custom_testing = False
+    
+    if len(sys.argv) == 3:
+        # If a class name is provided as an argument, use it
+        classes = [sys.argv[1]]
+        index_range = [int(sys.argv[2])]
+        custom_testing = True
+    else:
+        # Otherwise, use the default classes and index range
+        classes = ['adder', 'bv', 'mul', 'qaoa', 'qft', 'qpe', 'qsc', 'qv']
+        index_range = range(6)
+       
+        
+    # classes = ['adder', 'bv', 'mul', 'qaoa', 'qft', 'qpe', 'qsc', 'qv']
+    
+    max_qubits = 8
 
     results = {}
 
     for class_name in tqdm(classes):
         results[class_name] = {
-            'speedup': [],
-            'fidelity_difference': []
+            'speedup': {},
+            'fidelity_difference': {}
         }
         
-        for index in tqdm(range(6), leave=False):
+        for index in tqdm(index_range, leave=False):
 
             command, nqubits = get_command(class_name, index)
             
-            if nqubits > max_qubits:
+            if not custom_testing and nqubits > max_qubits:
                 break # skip all the rest benchmarks in this class since they will be too slow
 
             output = subprocess.check_output(command, shell=True)
 
             # Decode bytes to string
             output_str = output.decode("utf-8")
-            
-            print(output_str)
 
             # Split the output into lines and get the last three
             lines = output_str.strip().split("\n")[-3:]
@@ -36,8 +50,27 @@ def main():
 
             fidelity_difference = abs(float(lines[-2]) - float(lines[-1]))
             
-            results[class_name]['speedup'].append(speedup)
-            results[class_name]['fidelity_difference'].append(fidelity_difference)
+            results[class_name]['speedup'][index] = speedup
+            results[class_name]['fidelity_difference'][index] = fidelity_difference
+    
+    # check if the results file already exists
+    try:
+        with open('results.json', 'r') as f:
+            existing_results = json.load(f)
+            
+        # merge the new results with the existing ones
+        for class_name in results:
+            if class_name not in existing_results:
+                existing_results[class_name] = results[class_name]
+            else:
+                for index in results[class_name]['speedup']:
+                    existing_results[class_name]['speedup'][index] = results[class_name]['speedup'][index]
+                    existing_results[class_name]['fidelity_difference'][index] = results[class_name]['fidelity_difference'][index]
+        # save the merged results
+        results = existing_results    
+    except FileNotFoundError:
+        # if the file doesn't exist, just save the new results
+        pass
     
     with open('results.json', 'w') as f:
         json.dump(results, f, indent=4)
